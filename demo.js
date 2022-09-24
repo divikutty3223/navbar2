4e404afbc22d49f446ed04a414741b4a013b78dd
divikutty <divikutty3223@gmail.com>
2022-09-24 05:56:13 -0700

    dom

---
+++ demo.js
@@ -0,0 +1,6 @@
+var one =document.getElementById("one")
+one.innerHTML="hello"
+one.style.background="blue"
+var two =document.creatElementById("two")
+// two.src ="document.getElementById("two")
+two.src ="c:\Users\ELCOT\Desktop\dv\images.jpg"
---
+++ nav.js
@@ -0,0 +1,14 @@
+// DOM = Domcument object model
+//We can use the dom to manipulate html elements and change their behaviour.
+
+
+var head = document.getElementById("head")
+head.innerHTML="test"
+head.style.background="yellow"
+
+
+//Methodstg
+
+// 1.Hidden
+// 2.style
+// 3.Get a  image tag assign src value in JS
